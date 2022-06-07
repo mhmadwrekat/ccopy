3a5d3f4f6c6b334699854501bd3ff0c1f2155bf4
mhmadwrekat <mhmmadwreekkat@gmail.com>
2022-06-07 13:32:44 +0300

    update

---
+++ components/apple_template/child_comp/Menu_three_dot.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Menu } from '@headlessui/react'
+
+const Test = ({ title_color }) => {
+  return (
+    <React.Fragment>
+      <div className="relative">
+        <Menu as="div" className="inline-block text-left">
+          <Menu.Button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-9 w-9 cursor-pointer "
+              viewBox="0 0 20 20"
+              fill="#686767"
+            >
+              <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+            </svg>
+          </Menu.Button>
+          <Menu.Items>
+            <section className="absolute top-3 left-8 w-32">
+              <section className="text-black bg-white grid w-auto rounded-lg p-1 text-right font-TSbold text-sm ">
+                <Menu.Item>
+                  {({ active }) => (
+                    <p
+                      className={`border-b-1 border-GRAY200 py-1 px-2 ${
+                        active && `${title_color}`
+                      }`}
+                      href=""
+                    >
+                      نسخ الرابط
+                    </p>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <p
+                      className={`border-b-1 border-GRAY200 py-1 px-2 ${
+                        active && `${title_color}`
+                      }`}
+                      href=""
+                    >
+                      مشاركه
+                    </p>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <p
+                      className={`py-1 px-2 ${active && `${title_color}`}`}
+                      href=""
+                    >
+                      ابلاغ
+                    </p>
+                  )}
+                </Menu.Item>
+              </section>
+            </section>
+          </Menu.Items>
+        </Menu>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Test
--- components/page/HeadComp.js
+++ components/page/HeadComp.js
@@ -210,8 +210,12 @@ const HeadComp = () => {
         />
         <link
           rel="stylesheet"
-          href="path/to/font-awesome/css/font-awesome.min.css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         />
+        {/* <link
+          rel="stylesheet"
+          href="path/to/font-awesome/css/font-awesome.min.css"
+        /> */}
       </Head>
     </div>
   )
